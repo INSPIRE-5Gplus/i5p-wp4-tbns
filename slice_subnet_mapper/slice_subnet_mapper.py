9f53d76a6bf1318a7ef505f5a688a43651ec2123
alemanyp <paprats8@gmail.com>
2020-12-07 16:28:42 +0100

    Added functions to instantiate/terminate/get slice instances in the slice_mapper file.

--- slice_subnet_mapper/slice_subnet_mapper.py
+++ slice_subnet_mapper/slice_subnet_mapper.py
@@ -25,20 +25,29 @@ def get_slice_template(slice_ID):
     response = requests.get(url, headers=JSON_CONTENT_HEADER)
     return response.text, response.status_code
 
-#TODO: returns all instances information in the NSM
+# returns all instances information in the NSM
 def get_all_slice_instances():
-    
-    return 200
+    url = get_nsm_url() + "/slice-instances"
+    response = requests.get(url, headers=JSON_CONTENT_HEADER)
+    return response.text, response.status_code
 
-#TODO: returns specific instance information in the NSM
-def get_slice_instance(sliceID):
-    
-    return 200
+# returns specific instance information in the NSM
+def get_slice_instance(instance_ID):
+    url = get_nsm_url() + "/slice-instances/" + str(instance_ID)
+    response = requests.get(url, headers=JSON_CONTENT_HEADER)
+    return response.text, response.status_code
 
-#TODO: sends request to deploy a NST to the NSM
-def instantiate_slice():
-    return 200
+# sends request to deploy a NST to the NSM
+def instantiate_slice(slice_data):
+    # slice_data = {"instance_uuid": slice_template_uuid, "request_type": "CREATE_SLICE"}
+    url = get_nsm_url() + "/requests"
+    response = requests.post(url, headers=JSON_CONTENT_HEADER, data=slice_data)
+    return response.text, response.status_code
 
-#TODO: sends request to terminate a NST to the NSM
-def terminate_slice():
+# sends request to terminate a NST to the NSM
+def terminate_slice(slice_data):
+    # slice_data = {"instance_uuid": instance_uuid, "request_type": "TERMINATE_SLICE"}
+    url = get_nsm_url() + "/requests"
+    response = requests.post(url, headers=JSON_CONTENT_HEADER, data=slice_data)
+    return response.text, response.status_code
     return 200
