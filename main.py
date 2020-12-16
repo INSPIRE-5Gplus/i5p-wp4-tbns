936f6f6f1aaa7adc19d9ca8652db2c085e434604
alemanyp <paprats8@gmail.com>
2020-12-16 12:58:06 +0100

    Commented non-used Thread lines to test initial deployment phase.

--- main.py
+++ main.py
@@ -108,9 +108,8 @@ if __name__ == '__main__':
   #TODO: define the thread that must listen for Blockchain events (/blockchain_node/events_manager.py)
   #TODO: once defined, declare and start it
   # Threads to handle requests (local and blockchain)
-  event_filter = contract.events.notifySliceInstanceActions.createFilter(fromBlock='latest')
-  worker_blockchain_events = Thread(target=event_loop, args=(event_filter, 10), daemon=True)
-  worker_requests = thread_requests_control()
+  #event_filter = settings.contract.events.notifySliceInstanceActions.createFilter(fromBlock='latest')
+  #worker_blockchain_events = Thread(target=event_loop, args=(event_filter, 10), daemon=True)
 
   # RUN MAIN SERVER THREAD
   app.run(debug=False, host='localhost', port=os.environ.get("PDL_SLICE_PORT"))
