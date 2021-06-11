ddf4239b21f25325e64025e31784edd0829fb5f9
alemanyp <paprats8@gmail.com>
2021-06-11 13:19:10 +0200

    Error in a comment within the config_env.env file.

--- config_files/settings.py
+++ config_files/settings.py
@@ -84,18 +84,15 @@ def init_thread_pool(workers):
 def init_abstract_context(sdn_ctrl_ip, sdn_ctrl_port, model):
     response = sdn_mapper.get_local_context(sdn_ctrl_ip, sdn_ctrl_port)
     print(model)
-    print("**********************************************")
-    print (response)
     if (model == "vnode"):
         abstracted_context = vl_computation.vnode_abstraction(response[0])
     elif (model == "vlink"):
         abstracted_context = vl_computation.vlink_abstraction(response[0])
     elif (model == "transparent"):
         abstracted_context = response[0]
-        print("**********************************************")
-        print(abstracted_context)
     else:
         print("Wrong abstraction model selected. Validate [ABSTRACION_MODEL] in the configuration file to be one of these: vnode, vlink, transparent.")
+        abstracted_context = {"msg": "Wrong abstraction model selected. It is not programmed."}
         pass
     response = db.add_element("context", abstracted_context)
 
